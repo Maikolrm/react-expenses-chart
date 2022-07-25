c850678a78efd3e17b220707d1bdacef8f702893
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-25 16:22:16 -0400

    Project - App - adding local state and fetching chart data

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import { useState, useEffect } from "react"
 import Chart from "./components/Chart"
 
 function App() {
+  // locat state
+  const [data, setData] = useState([])
+
   // first mount
   useEffect(() => {
     async function fetchData() {
       try {
-        console.log("we are ready to go......")
+        const response = await fetch("/data.json")
+        const data = await response.json()
+        setData(data)
       } catch(e) {
         console.log(e)
       }
@@ -27,7 +32,7 @@ function App() {
       </div>
       <div className="p-6 mt-4 bg-pale-orange rounded-lg">
         <h2 className="text-lg font-bold text-dark-brown leading-none">Spending - Last 7 days</h2>
-        <Chart data={[]} />
+        <Chart data={data} />
       </div>
     </main>
   )
