8d3149b1d6334a393bf0f9f2d423f407909bc8e9
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-25 16:49:48 -0400

    Components - Chart - passing new property to ChartBar component ( expenses )

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -3,7 +3,7 @@ function ChartBar(props) {
   return (
     <div className="flex-1">
       <div className="h-64 bg-soft-red rounded-md"></div>
-      <p className="mt-4 text-sm text-center text-mid-brown leading-none">label</p>
+      <p className="mt-4 text-sm text-center text-mid-brown leading-none">{props.expense.day}</p>
     </div>
   )
 }
@@ -11,7 +11,7 @@ function ChartBar(props) {
 function Chart({ expenses }) {
   return (
     <div className="mt-4 flex gap-3">
-      {expenses.map(expense => <ChartBar key={expense.day} />)}
+      {expenses.map(expense => <ChartBar key={expense.day} expense={expense} />)}
     </div>
   )
 }
