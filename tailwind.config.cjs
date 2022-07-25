04dbb41fe95b807807a000e394562acec8e1e54a
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-25 16:21:33 -0400

    Project - tailwind - fixing typo on extended color ( cyan )

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     extend: {
       colors: {
         "soft-red": "hsl(10, 79%, 65%)",
-        "cyan": "Cyan: hsl(186, 34%, 60%)",
+        "cyan": "hsl(186, 34%, 60%)",
         "dark-brown": "hsl(25, 47%, 15%)",
         "mid-brown": "hsl(28, 10%, 53%)",
         "cream": "hsl(27, 66%, 92%)",
