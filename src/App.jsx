07b405ec95c6b3907ba937596c8bc5860f190cff
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-07-25 15:22:21 -0400

    Project - App - changing containers bg color, paddings and border radius

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function ChartBar() {
   return (
     <div className="">
       <div className="w-10 h-64 bg-soft-red rounded-md"></div>
-      <p className="mt-4 text-sm text-center leading-none">label</p>
+      <p className="mt-4 text-sm text-center text-mid-brown leading-none">label</p>
     </div>
   )
 }
@@ -24,15 +24,15 @@ function App() {
 
   return (
     <main className="m-auto w-full max-w-md">
-      <div className="p-4 rounded-lg bg-dark-brown text-red-400 flex">
+      <div className="p-6 rounded-lg bg-soft-red text-red-400 flex items-center text-white">
         <div className="flex-1">
           <h2>My balance</h2>
           <p>$921.48</p>
         </div>
         <div>Logo</div>
       </div>
-      <div className="p-4 mt-4 bg-cream rounded-lg">
-        <h2>Spending - Last 7 days</h2>
+      <div className="p-6 mt-4 bg-pale-orange rounded-lg">
+        <h2 className="text-lg font-bold text-dark-brown">Spending - Last 7 days</h2>
         <div className="mt-4 flex justify-between">
           <ChartBar />
           <ChartBar />
